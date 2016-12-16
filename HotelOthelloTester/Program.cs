56db28c404b50e3e06f155f22398f45a045e880c
joaquim <joaquim.perez@he-arc.ch>
2016-12-16 13:00:58 +0100

    nouveau projet HotelOthelloTester qui permet d'utiliser les classes Tile et Board de HoteOthello en console

---
+++ HotelOthelloTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HotelOthello;
+
+namespace HotelOthelloTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Console.WriteLine(board.ToString());
+            board.Tiles[2, 5].IsTaken = true;
+            Console.WriteLine(board.ToString());
+            Console.ReadKey();  
+        }
+    }
+}
