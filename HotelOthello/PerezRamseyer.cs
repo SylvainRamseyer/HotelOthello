65bd21428178030b6ef9d61964990d264464db71
SylvainRamseyer <sylvain.ramseyer@gmail.com>
2017-02-27 09:11:54 +0100

    matrix de poids

--- HotelOthello/PerezRamseyer.cs
+++ HotelOthello/PerezRamseyer.cs
@@ -8,6 +8,31 @@ namespace HotelOthello
 {
     class PerezRamseyer
     {
+
+        /*
+         * matrice d'indication des poids de chaque cases
+        500  -150 30 10 10 30 -150 500
+        -150 -250 0  0  0  0  -250 -150
+        30   0    1  2  2  1  0    30
+        10   0    2  16 16 2  0    10
+        10   0    2  16 16 2  0    10
+        30   0    1  2  2  1  0    30
+        -150 -250 0  0  0  0  -250 -150
+        500  -150 30 10 10 30 -150 500
+        */
+        public static readonly int[,] WEIGHT_MATRIX = {
+            {500, -150, 30, 10, 10, 30, -150, 500},
+            {-150, -250, 0, 0, 0, 0, -250, -150},
+            {30, 0, 1, 2, 2, 1, 0, 30},
+            {10, 0, 2, 16, 16, 2, 0, 10},
+            {10, 0, 2, 16, 16, 2, 0, 10},
+            {30, 0, 1, 2, 2, 1, 0, 30},
+            {-150, -250, 0, 0, 0, 0, -250, -150},
+            {500, -150, 30, 10, 10, 30, -150, 500}
+        };
+
+
+
         /// <summary>
         /// Asks the game engine next (valid) move given a game position
         /// The board assumes following standard move notation:
@@ -31,7 +56,25 @@ namespace HotelOthello
         /// <returns></returns>
         Tuple<char, int> getNextMove(int[,] game, int level, bool whiteTurn)
         {
-
+            return null;
         }
+
+
+        /*def alphabeta2(root , depth , minOrMax , parentValue) :
+        # minOrMax = 1 : maximize
+        # minOrMax = 􀀀1 : minimize
+            if(depth == 0 or root . final () ) :
+            return root.eval() , None
+            optVal = minOrMax  􀀀inf
+            optOp = None
+            for op in root.ops() :
+            new = root.apply(op)
+            val, dummy = alphabeta2(new , depth􀀀1,􀀀  - minOrMax, optVal)
+            if val  minOrMax > optVal  minOrMax :
+                optVal , optOp = val , op
+                if optVal  minOrMax > parentValue - minOrMax :
+                    break
+            return optVal , optOp */
+
     }
 }
